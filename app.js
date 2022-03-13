d5d9b5051b54b58b8e9f02ed8a4907a4ee303322
Ranjan Saha <ranjan.kailash@gmail.com>
2022-03-13 13:10:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var num = Math.floor(Math.random() * 4) + 1;
+$("#card").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/Gpay_Card_${num}.jpg`, // Background (image path or hex color).
+  fg: `Images/front.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
